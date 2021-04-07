9969123feb24565d90c1ebf3cef9cef1210d71a4
Disha S <shettydisha85@gmail.com>
2021-04-07 21:18:48 +0530

    Commit V1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-
-const routes: Routes = [];
-=======
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -14,7 +10,6 @@ const routes: Routes = [
   {path:'Login',component:LoginComponent},
   {path:'Register',component:RegisterComponent}
 ];
->>>>>>> 61e3fdd0a76a6a8db943812be9e5d658b78f60aa
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-=======
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
@@ -36,7 +26,6 @@ import { AdminComponent } from './components/admin/admin.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule
->>>>>>> 61e3fdd0a76a6a8db943812be9e5d658b78f60aa
   ],
   providers: [],
   bootstrap: [AppComponent]
