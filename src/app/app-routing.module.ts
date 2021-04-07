105b6480857cfdd89d006d9859f6048363670293
Disha S <shettydisha85@gmail.com>
2021-04-07 20:46:40 +0530

    After First Pull

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< HEAD
 
 const routes: Routes = [];
+=======
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent,pathMatch:"full"},
+  {path:'Home',component:HomeComponent},
+  {path:'Login',component:LoginComponent},
+  {path:'Register',component:RegisterComponent}
+];
+>>>>>>> 61e3fdd0a76a6a8db943812be9e5d658b78f60aa
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 
 @NgModule({
   declarations: [
@@ -11,6 +12,31 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+=======
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminComponent } from './components/admin/admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationBarComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+>>>>>>> 61e3fdd0a76a6a8db943812be9e5d658b78f60aa
   ],
   providers: [],
   bootstrap: [AppComponent]
