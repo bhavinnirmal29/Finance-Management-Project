fbf21e99fa5869627ed89911acdd82905e7df29b
Bhavin <bhavinnirmal29@gmail.com>
2021-04-08 09:45:38 +0530

    Admin UI Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -8,7 +9,8 @@ const routes: Routes = [
   {path:'',component:HomeComponent,pathMatch:"full"},
   {path:'Home',component:HomeComponent},
   {path:'Login',component:LoginComponent},
-  {path:'Register',component:RegisterComponent}
+  {path:'Register',component:RegisterComponent},
+  {path:'Admin',component:AdminComponent}
 ];
 
 @NgModule({
--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
-
+  name:string;
+  pwd:string;
+  model:any=[];
+  constructor() {}
   ngOnInit(): void {
+  }
+  AdminLogin(AdminForm:NgForm):void{
+    this.name =AdminForm.value.userName;
+    this.pwd=AdminForm.value.pwd;
+    console.log(this.name+" "+this.pwd);
   }
 
 }
